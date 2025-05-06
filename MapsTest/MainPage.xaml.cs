0d54e6eec844315a4d1cd5e61745ea3bbc5017e5
Michael Clayton <michael.clayton@royalmail.com>
2025-05-06 09:32:10 +0100

    Initial commit

---
+++ MapsTest/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MapsTest;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PushAsync(new MapPage());
+    }
+}
---
+++ MapsTest/MapPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MapsTest;
+
+public partial class MapPage : ContentPage
+{
+	public MapPage()
+	{
+		InitializeComponent();
+	}
+}
